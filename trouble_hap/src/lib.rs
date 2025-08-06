014649dd846c77d797546b0b217d81866825cddb
Ivor Wanders <ivor@iwanders.net>
2025-08-06 18:20:08 -0400

    Start of the homekit uuid.

--- trouble_hap/src/lib.rs
+++ trouble_hap/src/lib.rs
@@ -1,4 +1,23 @@
-#![no_std]
+#![cfg_attr(not(test), no_std)]
 use heapless::String;
 
+#[cfg(test)]
+extern crate std;
+
 use trouble_host::prelude::*;
+pub mod uuid;
+
+/// Accessory Information
+#[gatt_service(uuid = uuid::HomekitUuid16::new(0x003e))]
+//#[gatt_service(uuid = BluetoothUuid16::new(0xFED3))]
+pub struct AccessoryInformationService {
+    // 00000053-0000-1000-8000-0026BB765291
+    // 00000030-0000-1000-8000-0026BB765291
+    // E604E95D-A759-4817-87D3-AA005083A0D1
+    // 00000021-0000-1000-8000-0026BB765291
+    // 00000023-0000-1000-8000-0026BB765291
+    // 34AB8811-AC7F-4340-BAC3-FD6A85F9943B
+    // 00000020-0000-1000-8000-0026BB765291
+    // 00000052-0000-1000-8000-0026BB765291
+    // 00000014-0000-1000-8000-0026BB765291
+}
---
+++ trouble_hap/src/uuid.rs
@@ -0,0 +1,94 @@
+use trouble_host::attribute::Uuid;
+
+// definitely pfilfered from the trouble uuid module.
+//
+pub const HOMEKIT_BASE_UUID_BYTES: [u8; 16] = [
+    0x00, 0x00, 0x00, 0x00, // end of first block
+    0x00, 0x00, 0x10, 0x00, 0x80, 0x00, 0x00, 0x26, 0xBB, 0x76, 0x52, 0x91,
+];
+
+pub const HOMEKIT_BASE_UUID: Uuid = Uuid::new_long(HOMEKIT_BASE_UUID_BYTES);
+
+/// Bluetooth UUID.
+#[repr(transparent)]
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct HomekitUuid16(u16);
+
+impl HomekitUuid16 {
+    /// Create a new `BluetoothUuid16`.
+    pub const fn new(uuid: u16) -> Self {
+        Self(uuid)
+    }
+    /// Convert the `BluetoothUuid16` to a byte array as a const function.
+    pub const fn to_le_bytes(self) -> [u8; 2] {
+        self.0.to_le_bytes()
+    }
+    /// Convert from a byte array to a `BluetoothUuid16`.
+    pub const fn from_le_bytes(bytes: [u8; 2]) -> Self {
+        Self(u16::from_le_bytes(bytes))
+    }
+    pub const fn to_le_bytes_long(&self) -> [u8; 16] {
+        let mut raw_bytes = HOMEKIT_BASE_UUID_BYTES;
+        let [high, low] = self.to_le_bytes();
+        raw_bytes[3] = high;
+        raw_bytes[2] = low;
+        raw_bytes
+    }
+}
+
+impl From<HomekitUuid16> for u16 {
+    fn from(uuid: HomekitUuid16) -> u16 {
+        uuid.0
+    }
+}
+
+impl From<HomekitUuid16> for [u8; 2] {
+    fn from(uuid: HomekitUuid16) -> [u8; 2] {
+        uuid.0.to_le_bytes()
+    }
+}
+
+impl core::fmt::Debug for HomekitUuid16 {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "HomekitUuid16(0x{:04X})", self.0)
+    }
+}
+
+impl core::fmt::Display for HomekitUuid16 {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "0x{:04X}", self.0)
+    }
+}
+
+#[cfg(feature = "defmt")]
+impl defmt::Format for HomekitUuid16 {
+    fn format(&self, f: defmt::Formatter) {
+        defmt::write!(f, "BluetoothUuid16(0x{:04X})", self.0)
+    }
+}
+
+impl From<HomekitUuid16> for Uuid {
+    fn from(uuid: HomekitUuid16) -> Uuid {
+        // "0000xxxx-0000-1000-8000-0026BB765291"
+        Uuid::new_long(uuid.to_le_bytes_long())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_ble_uuid() {
+        const SERVICE_UUID: HomekitUuid16 = HomekitUuid16::new(0x003E);
+        assert_eq!(u16::from(SERVICE_UUID), 0x003E);
+        let uuid: u16 = SERVICE_UUID.into();
+        assert_eq!(uuid, 0x003E);
+        const UUID: [u8; 2] = SERVICE_UUID.to_le_bytes();
+        assert_eq!(UUID, [0x3E, 0x00]);
+        let full_uuid: Uuid = SERVICE_UUID.into();
+        if let Uuid::Uuid128(z) = full_uuid {
+            assert_eq!(z[3], 0x3e);
+        }
+    }
+}
