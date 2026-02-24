d775384a648b6bf7816d9cb2fb5222012e785d28
Ivor Wanders <ivor@iwanders.net>
2026-02-24 18:56:07 -0500

    Scaffolding for analysis package.

---
+++ analysis_imu/src/lib.rs
@@ -0,0 +1,9 @@
+use firmware_imu::icm42688;
+use firmware_imu::icm42688::ICM42688;
+use firmware_imu::lsm6dsv320x;
+use firmware_imu::lsm6dsv320x::LSM6DSV320X;
+
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("foo");
+    Ok(())
+}
